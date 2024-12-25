72857d191e8269147816c7315257e19a685d0711
ianamldv <ianamldv@gmail.com>
2024-12-26 01:00:25 +0200

    add text to dashboard

--- pages/1_dashboard.py
+++ pages/1_dashboard.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
-st.write('#Dashboard')
+# Display introductory message
+st.markdown(
+    """
+    **Hi, dear student!**  
+    Welcome to your personalized learning assistant. This system leverages **advanced AI and machine learning techniques** to:
+    
+    - Process documents and extract valuable data
+    - Help create customized study schedules
+    - Predict your success based on various factors
+    
+    All of this is designed to support your learning journey and optimize your academic success. Let’s get started!
+    """
+)
+
+st.header("Data Analysis Insights ")
+
+st.write("""
+    **Based on the dataset we had:**    
+    -the young perform better
+    -the more you study the better you perform
+    -going to private school doesn't make the students perform better
+    -students without a job perform better
+    -     
+""")
